0b9eb0a50a9100110bf082825a4bfeb9c51327c6
GabrieleSalati <gabriele.salati5@gmail.com>
2023-02-22 10:06:31 +0100

    Dynamically generated links added

--- js/app.js
+++ js/app.js
@@ -3,7 +3,14 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+
+            // HEADER LINKS ARRAY
+
+            head_links: ['Home', 'Who we are', 'What we do', 'Where we Work', 'Careers', 'News'],
+
+            // FOOTER LINKS ARRAY
+
+            bot_links: ['Home', 'Who we are', 'What we do', 'Where we work', 'Careers', 'Get in touch now']
         }
     }
 }).mount('#app')
